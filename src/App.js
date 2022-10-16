4376ad648a3738cc46f96c814ab09c0e0acfbd7b
Dheerendra-01 <dheerendra@plazmateq.com>
2022-10-16 08:33:30 +0530

    Home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+const App = () => {
+  const [news, setNews] = useState();
+  const [loading, setLoading] = useState(true);
+  const BASE_URL =
+    "https://newsapi.org/v2/top-headlines?country=us&apiKey=f29af06945f242688773dfc126faa5a6";
 
-function App() {
+  useEffect(() => {
+    axios.get(BASE_URL).then((result) => {
+      const response = result.data?.articles;
+      setNews(response);
+      setLoading(false);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {loading ? (
+        <>
+          <h1>Loading....</h1>
+        </>
+      ) : (
+        <>
+          <h1>Latest News</h1>
+          <div className="Container">
+            {news?.map((val) => {
+              return (
+                <>
+                  <div className="cards">
+                    <img src={val.urlToImage} alt="" srcset="" /> <br />
+                    <p style={{ fontWeight: "bold", color: "blue" }}>
+                      {val.publishedAt}
+                    </p>
+                    <h5>{val.title}</h5>
+                    <p>{val.description}</p>
+                  </div>
+                </>
+              );
+            })}
+          </div>
+        </>
+      )}
+    </>
   );
-}
+};
 
 export default App;
